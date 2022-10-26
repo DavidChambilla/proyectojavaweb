d0bff566833f80c754f030542814f1646db25a66
DavidChambilla <anvor127@gmail.com>
2022-10-26 08:15:00 -0500

    rol entity
    atributos

--- proyecto/ClbStore/src/main/java/pe/com/clbstore/entity/RolEntity.java
+++ proyecto/ClbStore/src/main/java/pe/com/clbstore/entity/RolEntity.java
@@ -1,9 +1,42 @@
-
 package pe.com.clbstore.entity;
 
+import java.io.Serializable; 
+import javax.persistence.Column; 
+import javax.persistence.Entity; 
+import javax.persistence.GeneratedValue; 
+import javax.persistence.GenerationType; 
+import javax.persistence.Id; 
+import javax.persistence.Table;
+import lombok.AllArgsConstructor; 
+import lombok.Builder; 
+import lombok.Data; 
+import lombok.NoArgsConstructor;
 
-public class RolEntity {
-    private int Codigo;
-    private String nombre;
+//@Builder te permite generar la construccion de la clase
+@Builder
+//@AllArgsConstructor te permite generar el metodo constructo con parametros
+@AllArgsConstructor
+//@NoArgsConstructor te permite generar el metodo constructor sin parametros
+@NoArgsConstructor
+//@Data te permite generar los metodos get y set
+@Data
+//@Entity define la entidad con la que se va a trabajar
+@Entity(name="RolEntity")
+//@Table define el nombre de la tabla con la cual se va a trabajar
+@Table(name="distrito") 
+
+public class RolEntity implements Serializable{
+    //agregamos los atributos
+    //agregamos el id de serializacion
+    private static final long serialVersionUID=1L;
+    @Id
+    @Column(name="idrol")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name="rol")
+    private String rol;
+    @Column(name="descriprol")
+    private String descripcion;
+    @Column(name="est")
     private boolean estado;
 }
