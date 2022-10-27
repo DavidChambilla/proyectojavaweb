3fea186e607cbcdbcea79a9389f34e73ed4682af
71762257 <71762257@certus.edu.pe>
2022-10-26 19:47:22 -0500

    DetalleVenta
    xD

---
+++ proyecto/ClbStore/src/main/java/pe/com/clbstore/repository/DetalleVentaRepository.java
@@ -0,0 +1,14 @@
+package pe.com.clbstore.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pe.com.clbstore.entity.DetalleVentaEntity;
+
+
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVentaEntity, Long>{
+    @Query("select c from DetalleVentaEntity c where c.estado=1")
+    List<DetalleVentaEntity> findAllCustom();   
+}
+
